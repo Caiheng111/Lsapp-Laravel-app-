e531b0cdad565ae86a4cabad2f00803e527c9f94
Cai heng <ianyaheng822@gmail.com>
2020-03-06 12:29:26 +1100

    setting the dev env

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{config('app.name','LSAPP')}}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{config('app.name','LSAPP')}}</title>
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+<body>
+
+  @include('inc.navbar')
+
+  <div class="container">
+      @yield('content')
+  </div>
+
+  
+
+
+</body>
+
+</html>
